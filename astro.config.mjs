2a84cc138e38d8c9e0bc416b0acd04960f906883
yuhexin <yuhexin25@gmail.com>
2026-06-27 19:47:25 -0400

    Fix GitHub Pages base paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://yuhexin25.github.io',
+  site: 'https://yuhexin25.github.io/hexinyu',
   base: '/hexinyu/',
 });
